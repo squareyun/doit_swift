e07973aaae0c9c2167ecb3b8ee9de683a8890bdb
squareyun <squareyun@gmail.com>
2022-05-07 01:25:04 +0900

    PickerView + Mission

---
+++ PickerViewMission/PickerViewMission/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  PickerViewMission
+//
+//  Created by 윤우혁 on 2022/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    let MAX_ARRAY_NUM = 10
+    let PICKER_VIEW_COLUMN = 2
+    let PICKER_VIEW_HEIGHT:CGFloat = 80
+    var imageArray = [UIImage?]()
+    var imageFileName = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"]
+    
+    @IBOutlet var pickerView: UIPickerView!
+    @IBOutlet var lblFileName: UILabel!
+    @IBOutlet var imageVIew: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        for i in 0 ..< MAX_ARRAY_NUM {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        lblFileName.text = imageFileName[0]
+        imageVIew.image = imageArray[0]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArray[row])
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 150)
+        
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (component == 0){
+            lblFileName.text = imageFileName[row]
+        } else {
+            imageVIew.image = imageArray[row]
+        }
+    }
+
+}
+
